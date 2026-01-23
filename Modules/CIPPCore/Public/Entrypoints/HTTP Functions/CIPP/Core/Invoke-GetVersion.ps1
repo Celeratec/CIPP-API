ad3230d946ab574fd9d294affb88a0d069592a4b
Clint Thomon <Clint.Thomson@Celeratec.com>
2026-01-22 18:33:34 -0600

    Add cleanup functionality for stale version entries in Invoke-GetVersion

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Core/Invoke-GetVersion.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/CIPP/Core/Invoke-GetVersion.ps1
@@ -8,6 +8,34 @@ Function Invoke-GetVersion {
     [CmdletBinding()]
     param($Request, $TriggerMetadata)
     $CIPPVersion = $request.query.LocalVersion
+    $CleanupStale = $request.query.CleanupStale -eq 'true'
+
+    # If cleanup requested, remove stale version entries first
+    if ($CleanupStale) {
+        try {
+            $VersionTable = Get-CippTable -tablename 'Version'
+            $AllVersions = Get-CIPPAzDataTableEntity @VersionTable -Filter "PartitionKey eq 'Version'"
+            
+            foreach ($ver in $AllVersions) {
+                # Remove entries with very old versions (before v7.0.0) or invalid versions
+                if ($ver.Version) {
+                    try {
+                        $semVer = [semver]$ver.Version
+                        if ($semVer.Major -lt 7) {
+                            Write-Information "Removing stale version entry: $($ver.RowKey) = $($ver.Version)"
+                            Remove-AzDataTableEntity @VersionTable -Entity $ver
+                        }
+                    } catch {
+                        # Invalid semver - remove it
+                        Write-Information "Removing invalid version entry: $($ver.RowKey) = $($ver.Version)"
+                        Remove-AzDataTableEntity @VersionTable -Entity $ver
+                    }
+                }
+            }
+        } catch {
+            Write-Warning "Failed to cleanup stale versions: $($_.Exception.Message)"
+        }
+    }
 
     $Version = Assert-CippVersion -CIPPVersion $CIPPVersion
 
