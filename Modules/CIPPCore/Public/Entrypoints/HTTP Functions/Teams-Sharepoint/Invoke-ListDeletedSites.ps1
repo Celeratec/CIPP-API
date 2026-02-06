f4210b34be6d942d2f1ed486adad1dc88be680f7
Clint Thomon <Clint.Thomson@Celeratec.com>
2026-02-06 11:55:16 -0600

    Refactor Invoke-ListDeletedSites function to utilize CSOM ProcessQuery for retrieving deleted site properties from SharePoint. Update logic to handle response data and improve error handling for API calls, enhancing the overall functionality and reliability of the site listing process.

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListDeletedSites.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Teams-Sharepoint/Invoke-ListDeletedSites.ps1
@@ -19,28 +19,89 @@ function Invoke-ListDeletedSites {
     }
 
     try {
-        $DeletedSites = New-GraphGetRequest `
-            -uri 'https://graph.microsoft.com/v1.0/admin/sharepoint/deletedSites?$select=id,displayName,webUrl,deletedDateTime,siteCollection&$top=999' `
+        $SharePointInfo = Get-SharePointAdminLink -Public $false -tenantFilter $TenantFilter
+
+        # Use CSOM ProcessQuery to call Tenant.GetDeletedSitePropertiesFromSharePoint
+        # This is the same pattern used in Set-CIPPSharePointPerms.ps1
+        $XML = @"
+<Request xmlns="http://schemas.microsoft.com/sharepoint/clientquery/2009" AddExpandoFieldTypeSuffix="true" SchemaVersion="15.0.0.0" LibraryVersion="16.0.0.0" ApplicationName=".NET Library">
+  <Actions>
+    <ObjectPath Id="2" ObjectPathId="1" />
+    <ObjectPath Id="4" ObjectPathId="3" />
+    <Query Id="5" ObjectPathId="3">
+      <Query SelectAllProperties="true">
+        <Properties />
+      </Query>
+      <ChildItemQuery SelectAllProperties="true">
+        <Properties />
+      </ChildItemQuery>
+    </Query>
+  </Actions>
+  <ObjectPaths>
+    <Constructor Id="1" TypeId="{268004ae-ef6b-4e9b-8425-127220d84719}" />
+    <Method Id="3" ParentId="1" Name="GetDeletedSitePropertiesFromSharePoint">
+      <Parameters>
+        <Parameter Type="String">0</Parameter>
+      </Parameters>
+    </Method>
+  </ObjectPaths>
+</Request>
+"@
+
+        $Result = New-GraphPostRequest `
+            -scope "$($SharePointInfo.AdminUrl)/.default" `
             -tenantid $TenantFilter `
-            -asApp $true
+            -Uri "$($SharePointInfo.AdminUrl)/_vti_bin/client.svc/ProcessQuery" `
+            -Type POST `
+            -Body $XML `
+            -ContentType 'text/xml'
+
+        # Check for CSOM errors
+        $ErrorInfo = $Result | Where-Object { $_.ErrorInfo } | Select-Object -First 1
+        if ($ErrorInfo.ErrorInfo.ErrorMessage) {
+            throw "SharePoint API error: $($ErrorInfo.ErrorInfo.ErrorMessage)"
+        }
+
+        # Extract child items from the query result
+        $DeletedSiteData = @()
+        foreach ($Item in $Result) {
+            if ($Item._Child_Items_) {
+                $DeletedSiteData = $Item._Child_Items_
+                break
+            }
+        }
 
         $Now = Get-Date
-        $GraphRequest = foreach ($Site in $DeletedSites) {
-            $DeletedDate = if ($Site.deletedDateTime) { [datetime]$Site.deletedDateTime } else { $null }
-            $DaysRemaining = if ($DeletedDate) {
+        $GraphRequest = foreach ($Site in $DeletedSiteData) {
+            # DaysRemaining is provided directly by the CSOM API
+            $DaysRemaining = if ($null -ne $Site.DaysRemaining) {
+                [int]$Site.DaysRemaining
+            } elseif ($Site.DeletionTime) {
+                $DeletedDate = [datetime]$Site.DeletionTime
                 $ExpiryDate = $DeletedDate.AddDays(93)
                 [math]::Max(0, [math]::Ceiling(($ExpiryDate - $Now).TotalDays))
+            } else {
+                0
+            }
+
+            # Extract a display name from the URL (last segment)
+            $Url = $Site.Url
+            $DisplayName = if ($Url) {
+                $UrlParts = $Url.TrimEnd('/') -split '/'
+                $UrlParts[-1] -replace '%20', ' '
             } else {
                 'Unknown'
             }
 
             [PSCustomObject]@{
-                siteId          = $Site.id
-                displayName     = $Site.displayName
-                webUrl          = $Site.webUrl
-                deletedDateTime = $Site.deletedDateTime
+                siteId          = $Site.SiteId
+                displayName     = $DisplayName
+                webUrl          = $Url
+                deletedDateTime = $Site.DeletionTime
                 daysRemaining   = $DaysRemaining
-                siteCollection  = $Site.siteCollection
+                status          = $Site.Status
+                storageQuota    = $Site.StorageMaximumLevel
+                resourceQuota   = $Site.ResourceQuota
             }
         }
 
