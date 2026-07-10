4e585e9c9230c2077adc4189b9f526d9f1d10211
John Duprey <jwd@johnwduprey.com>
2026-07-09 22:08:40 -0400

    fix: add onprem sync attributes
    (cherry picked from commit 5d3befc8387ac18b627d0a35a1347c01ebf4d3fd)

--- Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ListUsers.ps1
+++ Modules/CIPPHTTP/Public/Entrypoints/HTTP Functions/Identity/Administration/Users/Invoke-ListUsers.ps1
@@ -24,7 +24,7 @@ Function Invoke-ListUsers {
             'employeeHireDate', 'employeeId', 'employeeLeaveDateTime', 'employeeType', 'faxNumber',
             'givenName', 'jobTitle', 'lastPasswordChangeDateTime', 'legalAgeGroupClassification', 'mail',
             'mailNickname', 'mobilePhone', 'officeLocation', 'onPremisesDistinguishedName',
-            'onPremisesLastSyncDateTime', 'onPremisesSyncEnabled', 'otherMails', 'postalCode',
+            'onPremisesImmutableId', 'onPremisesLastSyncDateTime', 'onPremisesSyncEnabled', 'otherMails', 'postalCode',
             'preferredLanguage', 'proxyAddresses', 'showInAddressList', 'state', 'streetAddress',
             'surname', 'usageLocation', 'userPrincipalName', 'userType'
         )
